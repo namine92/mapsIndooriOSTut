ab80f822476ecf63d508248e3a4101529f8dbcba
Amine <Amine@Amine.local>
2017-05-29 12:59:43 +0200

    first commit

---
+++ mapsIndoorTest/GPSProvider.swift
@@ -0,0 +1,9 @@
+//
+//  GPSProvider.swift
+//  mapsIndoorTest
+//
+//  Created by Amine on 18/05/2017.
+//  Copyright © 2017 MacBookPro. All rights reserved.
+//
+
+import Foundation
---
+++ mapsIndoorTest/MyCustomLocationsProvider.swift
@@ -0,0 +1,9 @@
+//
+//  MyCustomLocationsProvider.swift
+//  mapsIndoorTest
+//
+//  Created by Amine on 16/05/2017.
+//  Copyright © 2017 MacBookPro. All rights reserved.
+//
+
+import Foundation
